9ba548bfb38e323e3162b73e054a8825857ac70e
songjie <songjie@pythonera.com>
2020-07-16 15:14:54 +0800

    sdkios

---
+++ Assets/Script/SDK/SDKIOS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+public class SDKIOS : SDKDefault
+{
+
+#if UNITY_IOS
+    [DllImport("__Internal")]
+    private static extern void EvaluateOc();
+    public override void Share()
+    {
+        CoroutineHelper.Instance.WaitForEndOfFrame(() =>
+        {
+            string shareText = "I've reached level " + SaveModel.player.level + ", so interesting! Come and play Onet 3D with me! https://play.google.com/store/apps/details?id=com.connetly.puzzle.game";
+            new NativeShare().SetSubject("Subject goes here").SetText(shareText).Share();
+
+        });
+    }
+
+    public override void Evaluate()
+    {
+        EvaluateOc();
+    }
+#endif
+}
